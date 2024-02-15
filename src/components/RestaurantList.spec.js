3d76fdd4783cedaddcc0c08da8dadab00885c260
Parthdsi <parthd830@gmail.com>
2024-02-15 18:57:37 -0500

    both test cases pass

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,9 +1,30 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
-    expect(loadRestaurants).toHaveBeenCalled();
-}); });
+    it('loads restaurants on first render', () => {
+      const loadRestaurants = jest.fn().mockName('loadRestaurants');
+      const restaurants = [];
+      render(
+        <RestaurantList
+          loadRestaurants={loadRestaurants}
+          restaurants={restaurants}
+        />,
+    );
+      expect(loadRestaurants).toHaveBeenCalled();
+    });
+  }); 
+
+describe('RestaurantList', () => {
+    it('displays the restaurants', () => { const noop = () => {};
+    const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+  render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+  expect(screen.getByText('Sushi Place')).toBeInTheDocument();
+  expect(screen.getByText('Pizza Place')).toBeInTheDocument();
+  
+});
+});
+
