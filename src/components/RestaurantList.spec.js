d8b9584f43c796e06c927df5d1e8fcd2491adc55
Parthdsi <parthd830@gmail.com>
2024-02-15 19:01:15 -0500

    refactored tests

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -2,29 +2,26 @@ import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
-    it('loads restaurants on first render', () => {
-      const loadRestaurants = jest.fn().mockName('loadRestaurants');
-      const restaurants = [];
-      render(
-        <RestaurantList
-          loadRestaurants={loadRestaurants}
-          restaurants={restaurants}
-        />,
-    );
-      expect(loadRestaurants).toHaveBeenCalled();
-    });
-  }); 
-
-describe('RestaurantList', () => {
-    it('displays the restaurants', () => { const noop = () => {};
     const restaurants = [
-    {id: 1, name: 'Sushi Place'},
-    {id: 2, name: 'Pizza Place'},
-  ];
-  render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
-  expect(screen.getByText('Sushi Place')).toBeInTheDocument();
-  expect(screen.getByText('Pizza Place')).toBeInTheDocument();
-  
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
+      let loadRestaurants;
+      function renderComponent() {
+        loadRestaurants = jest.fn().mockName('loadRestaurants');
+        render(
+          <RestaurantList
+            loadRestaurants={loadRestaurants}
+            restaurants={restaurants}
+          />,
+    ); }
+    it('loads restaurants on first render', () => {
+        renderComponent();
+        expect(loadRestaurants).toHaveBeenCalled();
+    });
+    it('displays the restaurants', () => {
+        renderComponent();
+        expect(screen.getByText('Sushi Place')).toBeVisible();
+        expect(screen.getByText('Pizza Place')).toBeVisible();
+    });
 });
-});
-
