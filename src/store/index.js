a2d8e4ef418f4490d3d4caeb11477250cfd211e2
twliew <twliew@uwaterloo.ca>
2024-03-17 13:21:45 -0400

    109

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-
 import rootReducer from './reducers';
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import api from '../api';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunk.withExtraArgument(api),
+    ),
+);
 
 export default store;
